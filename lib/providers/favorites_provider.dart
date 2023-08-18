cbc52d11898cd7d153ba7fed31e56b62107d4f5a
alfism <alfialfarisi@gmail.com>
2023-08-18 09:20:19 +0700

    implement providers

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealNotifier() : super([]);
+
+  bool toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = state.contains(meal);
+
+    if (isExisting) {
+      state = state.where((element) => element.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favoritesMealsProvider =
+    StateNotifierProvider<FavoriteMealNotifier, List<Meal>>((ref) {
+  return FavoriteMealNotifier();
+});
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/models/meal.dart';
+import 'package:meals/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
-    required this.onToggleFavorite,
   });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoritesMealsProvider);
+
+    final isFavorited = favoriteMeals.contains(meal);
+
     return Scaffold(
         appBar: AppBar(title: Text(meal.title), actions: [
           IconButton(
             onPressed: () {
-              onToggleFavorite(meal);
+              bool wasAdded = ref
+                  .read(favoritesMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(wasAdded
+                      ? 'Added to favorites!'
+                      : 'Removed from favorites!'),
+                ),
+              );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(
+              isFavorited ? Icons.star : Icons.star_border,
+            ),
           )
         ]),
         body: SingleChildScrollView(
